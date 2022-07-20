ae256db349c4a8a146d693d15efa1ba95722a5f5
tmfmfmr321 <alstjd9414@naver.com>
2022-07-20 22:46:20 +0900

    .

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ def index():
 
     return render_template('index.html', menu=menu)
 
-
+# 춤 일치도 분석
 @app.route('/menu1', methods=['GET', 'POST'])
-def menu1():    # 춤 일치도 분석
+def menu1():
     menu = {'home': 0, 'menu1': 1, 'menu2': 0}
     if request.method == 'GET' :
         return render_template('menu1.html', menu=menu)
@@ -28,13 +28,16 @@ def menu1():    # 춤 일치도 분석
         # return render_template('menu2_res.html', 변수1=, 변수2=, ...)
         return render_template('menu1_res.html', menu=menu)
 
-
+# 춤 입히기
 @app.route('/menu2', methods=['GET', 'POST'])
-def menu2():    # 춤 입히기
+def menu2():
     menu = {'home': 0, 'menu1': 0, 'menu2': 1}
     if request.method == 'GET':
         return render_template('menu2.html', menu=menu)
     else:
+        def upload():
+            uploaded_files = request.files.getlist("file[]")
+            print(uploaded_files)
         # 변수1 = request.form['변수1']
         # 변수2 = request.form['변수2']
         # ...
@@ -45,4 +48,4 @@ def menu2():    # 춤 입히기
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000, debug=True)
+    app.run(host='0.0.0.0',port=5500, debug=True)
