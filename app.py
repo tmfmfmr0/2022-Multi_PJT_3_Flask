118a789be8cde9e57b15d1f55fec6d718b484e5d
Minseong S <alstjd9414@naver.com>
2022-07-22 16:52:20 +0900

    .

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, redirect, render_template, request, send_file
+from flask import current_app
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
@@ -11,13 +13,6 @@ def index():
     menu = {'home': 1, 'menu1': 0, 'menu2': 0}
 
     return render_template('index.html', menu=menu)
-
-# @app.route('/files', method=['post'])
-# def files():
-#     file = request.files['xxx']
-#     file.filename # abc.mp4
-#     path = '' + file.filename
-#     file.save(path)
 
 # 춤 일치도 분석
 @app.route('/menu1', methods=['GET', 'POST'])
@@ -43,57 +38,56 @@ def menu2():
     # background_image = './src_ref/background/*'
     if request.method == 'GET':
         dance_options = [
-            {'disp':'BTS - DNA', 'val':'DNA'},
-            {'disp':'Blackpink - Kill this love', 'val':'Kill this love'},
-            {'disp':'BTS - Dynamite', 'val':'Dynamite'},
-            {'disp':'SOF - Hey mama', 'val':'Hey mama'},
-            {'disp':'Aespa - Girls', 'val':'Girls'},
-            {'disp':'PSY - 강남스타일', 'val':'강남스타일'},
-            {'disp':'직접추가', 'val':'direct'}
+            {'disp':'BTS-Dynamite', 'val':'dynamite'},
+            {'disp':'SOF-Hey mama', 'val':'heymama'},
+            {'disp':'Aespa-Girls', 'val':'girls'},
+            {'disp':'PSY-강남스타일', 'val':'gangnam'},
+            {'disp':'직접선택', 'val':'direct1'}
         ]
         character_options = [
-            {'disp':'기본1', 'val':'basic2'},
+            {'disp':'기본1', 'val':'basic1'},
             {'disp':'기본2', 'val':'basic2'},
-            {'disp':'직접녹화', 'val':'record_vid'},
-            {'disp':'직접추가', 'val':'direct2'}
+            {'disp':'직접선택', 'val':'direct2'}
         ]
         background_options = [
             {'disp':'기본', 'val':'basic'},
             {'disp':'화려함', 'val':'fancy'},
-            {'disp':'직접추가', 'val':'direct3'}
+            {'disp':'직접선택', 'val':'direct3'}
         ]
-        
-        return render_template('menu2.html', menu=menu, 
-                                dance_options=dance_options,
-                                background_options=background_options, 
-                                character_options=character_options)
+        return render_template('menu2.html', menu=menu, dance_options=dance_options,
+                                character_options=character_options, background_options=background_options)
     else:
-        
-        #dance_video = request.form['']
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # def upload():
-        #     uploaded_files = request.files.getlist("file[]")
-        #     print(uploaded_files)
-        # # 변수1 = request.form['변수1']
-        # # 변수2 = request.form['변수2']
-        # # ...
-        # # 사용자함수(변수1, 변수2, ...)
-        
-        # # return render_template('menu2_res.html', 변수1=, 변수2=, ...)
-        return render_template('menu2.html', menu=menu)
+        dance_options = request.form['dance_option']
+        if not os.path.exists(os.path.join(current_app.root_path, 'static/upload')):
+            os.makedirs(os.path.join(current_app.root_path, 'static/upload'))
+        if dance_options == 'direct1':
+            f_mp4 = request.files['dance_upload']
+            file_mp4 = os.path.join(current_app.root_path, 'static/upload/') + f_mp4.filename
+            f_mp4.save(file_mp4)
+        else:
+            file_mp4 = os.path.join(current_app.root_path, 'static/dance/') + dance_options + '.mp4'
+
+        character_options = request.form['character_option']
+        if not os.path.exists(os.path.join(current_app.root_path, 'static/upload')):
+            os.makedirs(os.path.join(current_app.root_path, 'static/upload'))
+        if character_options == 'direct2':
+            f_cha_mp4 = request.files['character_upload']
+            file_cha_mp4 = os.path.join(current_app.root_path, 'static/upload/') + f_cha_mp4.filename
+            f_cha_mp4.save(file_cha_mp4)
+        else:
+            file_cha_mp4 = os.path.join(current_app.root_path, 'static/character/') + character_options + '.mp4'
+            
+        background_options = request.form['background_option']
+        if not os.path.exists(os.path.join(current_app.root_path, 'static/upload')):
+            os.makedirs(os.path.join(current_app.root_path, 'static/upload'))
+        if background_options == 'direct3':
+            bg_png = request.files['background_upload']
+            file_bg = os.path.join(current_app.root_path, 'static/upload/') + bg_png.filename
+            bg_png.save(file_bg)
+        else:
+            file_bg = os.path.join(current_app.root_path, 'static/character/') + background_options + '.png'
+            
+        return redirect('/menu2', menu=menu)
     
 
 
---
+++ test.py
@@ -0,0 +1,21 @@
+﻿#%%
+import os
+from PIL import Image
+
+path_char = './src_ref/character/character1'
+for filename in os.listdir(path_char):
+    with Image.open(path_char+'\\'+filename) as character:
+        print(character.size)
+        
+path_bg = './src_ref/background'
+for filename in os.listdir(path_bg):
+    with Image.open(path_bg+'\\'+filename) as background:
+        print(background.size)
+        
+path_dance = './src_ref/dance'
+for filename in os.listdir(path_dance):
+    with open(os.path.join(path_dance, filename), 'r') as dance:
+        print(dance)
+# %%
+f
+# %%
